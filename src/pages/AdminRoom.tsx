f98d6c2c8c14d0d9646d0e0cf2590758d9af18ad
Vinicyus <vinicyus346@gmail.com>
2021-06-28 17:00:24 -0300

    Class 5

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -2,6 +2,9 @@ import { useHistory, useParams } from 'react-router-dom';
 
 import deleteImg from '../assets/images/delete.svg';
 import logoImg from '../assets/images/logo.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
+
 import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
@@ -38,6 +41,18 @@ export function AdminRoom() {
     }
   }
 
+  async function handleCheckQuestionAsAnswered(questionId: string){
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
+  }
+
   return (
     <div id="page-room">
       <header>
@@ -57,24 +72,48 @@ export function AdminRoom() {
           { questions.length > 0 && <span>{questions.length} pergunta(s)</span>}     
         </div>        
 
-        {
-          questions.map(question => {
-            return (
-              <Question
-                key={question.id}
-                content={question.content}
-                author={question.author}
-              >
-                <button
-                  type="button"
-                  onClick={() => handleDeleteQuestion(question.id)}
-                >  
-                  <img src={deleteImg} alt="Remover pergunta" />
-                </button>
-              </Question>
-            );
-          })  
-        }
+        <div className="question-list">
+
+        
+          {
+            questions.map(question => {
+              return (
+                <Question
+                  key={question.id}
+                  content={question.content}
+                  author={question.author}
+                  isAnswered={Boolean(question.isAnswered)}
+                  isHighlighted={Boolean(question.isHighlighted)}
+                >
+                  {!question.isAnswered && (
+                    <>
+                      <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                      >  
+                        <img src={checkImg} alt="Marcar Pergunta como respondida" />
+                      </button>
+
+                      <button
+                        type="button"
+                        onClick={() => handleHighlightQuestion(question.id)}
+                      >  
+                        <img src={answerImg} alt="Dar destaque a pergunta" />
+                      </button>
+                    </>
+                  )}
+
+                  <button
+                    type="button"
+                    onClick={() => handleDeleteQuestion(question.id)}
+                  >  
+                    <img src={deleteImg} alt="Remover pergunta" />
+                  </button>
+                </Question>
+              );
+            })  
+          }
+        </div>
       </main>
     </div>
   );
